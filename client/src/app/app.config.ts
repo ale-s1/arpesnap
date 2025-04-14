e62ae6168419f3f28b57b3c85fcd151bc262a490
Ale-dev <acorrea7412@gmail.com>
2025-04-14 17:37:51 -0500

    add animations and toastr providers to app configuration

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,14 +2,17 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { authHttpInterceptorFn, provideAuth0 } from '@auth0/auth0-angular';
+import { provideToastr } from 'ngx-toastr';
 import { environment } from '../environments/environment';
 import { routes } from './app.routes';
-
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideAnimations(),
+    provideToastr(),
     provideAuth0(environment.auth),
     provideHttpClient(withInterceptors([authHttpInterceptorFn])),
   ],
